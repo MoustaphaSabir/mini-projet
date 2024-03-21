c36743fb334529522354bbd0a5891007e7953431
Moustapha Sabir <moustaphasabirmoussa61@gmail.com>
2024-03-21 13:18:48 +0100

    Devis de la formation

--- devis.php
+++ devis.php
@@ -38,34 +38,73 @@
         <ul>
             <li><a href="projet.php">Accueil</a></li>
             <li><a href="Inscription.html">inscription</a></li>
-            <li><a href="Accueil.html">Accueil</a></li>
+            <li><a href="devis.html">Devis</a></li>
         </ul>
     </header>
-    <div class="container">
+     <div class="container">
         <h2>Devis Formation Bureautique</h2>
     <table>
         <tr>
             <th>Module</th>
-            <th>Durée (heures)</th>
+            <th>Durée (Mois)</th>
             <th>Prix (EUR)</th>
         </tr>
         <tr>
             <td>Microsoft Word</td>
-            <td>20</td>
+            <td>3</td>
             <td>300</td>
         </tr>
-        <tr>
-            <td>Microsoft Access</td>
-            <td>20</td>
+      <td>Microsoft Excel</td>
+        <td>3</td>
+            <td>350</td>
+        </tr>
+        <td>Microsoft Acces</td>
+            <td>3</td>
             <td>350</td>
         </tr>
         <tr>
-            <td>Microsoft Excel</td>
-            <td>30</td>
+            <td>Microsoft PowerPoint</td>
+            <td>3</td>
             <td>400</td>
         </tr>
     </table>
-        <p>Total: <?php echo 300 + 350 + 400; ?> EURO</p>
-    </div>
+        <p>Total: <?php echo 300 + 350 +350 + 400; ?> EURO</p>
+     <tr>
+     </div>
+            <!-- <td>Microsoft Access</td> -->
+            <!-- <h1>Devis Formation</h1> -->
+    <!-- <form action="projet.php" method="POST">
+        <label for="nom">Nom:</label><br>
+        <input type="text" id="nom" name="nom" required><br>
+
+        <label for="email">Email:</label><br>
+        <input data-="" type="email" id="email" name="email" required><br>
+
+
+        <label for="telephone">Téléphone:</label><br>
+        <input type="tel" id="telephone" name="telephone" required>
+        <h2>Formations:</h2>
+        <input type="checkbox" id="word" name="formations[200]" value="Word">
+        <label for="word">Word</label><br>
+
+        <input type="checkbox" id="excel" name="formations[500]" value="Excel">
+        <label for="excel">Excel</label><br>
+
+        <input type="checkbox" id="powerpoint" name="formations[300]" value="PowerPoint">
+        <label for="powerpoint">PowerPoint</label><br>
+        
+        <input type="checkbox" id="acces" name="formations[200]" value="acces">
+        <label for="acces">Acces</label><br>
+
+        <label for="budget">Budget total prévu pour les formations:</label><br>
+        <input type="number" id="budget" name="budget" required><br><br>
+
+        <label for="date_debut">Date de début souhaitée:</label><br>
+        <input type="date" id="date_debut" name="date_debut" required><br><br>
+
+        <label for="commentaires">Commentaires:</label><br>
+        <textarea id="commentaires" name="commentaires" rows="4" cols="50"></textarea><br><br>
+        <input type="submit" value="Envoyer">
+    </form>    -->
 </body>
 </html>
